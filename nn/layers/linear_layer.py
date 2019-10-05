Fix backprop for layers

nn/layers/linear_layer.py
@@ -9,6 +9,7 @@ class LinearLayer(Layer):
         super(LinearLayer, self).__init__(parent)
         self.bias = Parameter(np.zeros((1, output_size), dtype=np.float32))
         self.weight = Parameter(np.zeros((input_size, output_size), dtype=np.float32)) # TODO create the weight parameter
+        self.input = 0
 
     def forward(self, data: np.ndarray) -> np.ndarray:
         """
@@ -18,8 +19,14 @@ class LinearLayer(Layer):
         """
         # 3.1) TODO do the linear layer
         # (n x d) (d x c) = (n x c)
-        # print("Input/Output size = ", self.weight.data.shape)
-        # print("Data size =", np.shape(data))
+        if (False):
+            print("Input/Output size = ", self.weight.data.shape)
+            print("Data size =", np.shape(data))
+
+        # Save this for backward
+        self.input = data
+        self.bias.zero_grad()
+        self.weight.zero_grad()
         return np.matmul(data, self.weight.data)+self.bias.data
 
     def backward(self, previous_partial_gradient: np.ndarray) -> np.ndarray:
@@ -29,7 +36,23 @@ class LinearLayer(Layer):
         :return: gradients wrt inputs
         """
         # 3.1) TODO do the backward step
-        # (n x c) x  (c x d) = (n x d)
+        # PrevGrd = (n x c)
+        # dW = (d x c)
+        # dB = (1 x c)
+        # dX = (n x d)
+        if (False):
+            print("Linear Layer")
+            print("Prev grad = ", previous_partial_gradient.shape)
+            print("Weight = ", self.weight.data.shape)
+            print("B = ", self.bias.data.shape)
+            print("Data = ", self.input.shape)
+            print("\n")
+
+        # Need to save gradient for dw and db
+        self.bias.grad=np.sum(previous_partial_gradient, axis=0)
+        self.weight.grad=np.matmul(self.input.T, previous_partial_gradient)
+
+        # Return dx
         #  dy     dy          d(wx+b)     dy
         # ---- = --------- X --------- = -------- X w^T
         #  dx     d(wx+b)      dx         d(wx+b)

nn/layers/relu_layer.py
@@ -8,19 +8,34 @@ class ReLULayer(Layer):
     def __init__(self, parent=None):
         super(ReLULayer, self).__init__(parent)
         self.grad = 0
+        # self.relu = np.vectorize(lambda x: x if (x>0) else 0)
+        # self.drelu = np.vectorize(lambda x: 1 if (x>0) else 0)
+        self.relu = np.vectorize(lambda x: x * (x>0))
+        self.drelu = np.vectorize(lambda x: 1.0 * (x>0))
+        self.input = 0
 
     def forward(self, data):
         # TODO
-        #Return f(in) shape = (n x d)
-
-        relu = np.vectorize(lambda x: x if (x>0) else 0)
-        drelu = np.vectorize(lambda x: 1 if (x>0) else 0)
-        self.grad = drelu(data)
-        return relu(data)
+        # Element-wise function
+        # Return f(in) shape = (n x d)
+        # self.grad=(data>=0)
+        # self.grad=self.drelu(data)
+        self.input = data
+        return self.relu(data)
 
     def backward(self, previous_partial_gradient):
         # TODO
-        return np.matmul(previous_partial_gradient, np.transpose(self.grad))
+        if (False):
+            print("RELU Layer")
+            print("Prev grad = ", previous_partial_gradient.shape)
+            print("Grad Weight = ", np.transpose(self.grad).shape)
+            print("\n")
+        #  dy     dy        df(z)
+        # ---- = ------- X -------
+        #  dz     df(z)     dz
+        # new_grad = dy/df(in) * df(in)/d(in)
+        return previous_partial_gradient*(self.input >=0)
+        # return np.multiply(previous_partial_gradient, self.grad)
 
     # def selfstr(self):
     #     return str(self.grad.shape)
@@ -33,6 +48,7 @@ class ReLUNumbaLayer(Layer):
     @njit(parallel=True, cache=True)
     def forward_numba(data):
         # 3.2) TODO
+        print("Running Numba\n")
         output = np.copy(data)
         output = output.flatten()
         relu = np.vectorize(lambda x: x if (x>0) else 0)
@@ -53,6 +69,7 @@ class ReLUNumbaLayer(Layer):
     @njit(parallel=True, cache=True)
     def backward_numba(data, grad):
         # 3.2) TODO
+        print("Numba relu grad = ", grad.shape)
         output = np.zeros(data.shape)
         output = output.flatten()
         drelu = np.vectorize(lambda x: 1 if (x>0) else 0)