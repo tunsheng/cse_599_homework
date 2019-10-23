1735ee249e46603f443fe91f62f6b80d0ba8f5ab
Tun Sheng <tunsheng@uw.edu>
2019-10-22 17:50:06 -0700

    Added linear layer

--- nn/layers/conv_layer.py
+++ nn/layers/conv_layer.py
@@ -15,7 +15,87 @@ class ConvLayer(Layer):
         self.kernel_size = kernel_size
         self.padding = (kernel_size - 1) // 2
         self.stride = stride
+        self.input = None
         self.initialize()
+
+    """
+    There is one bias per output channel, similar to the LinearLayer
+    (batch, input_channels, height, width)
+
+    padding is defined as how many rows to add to ALL 4 sides
+    of the input images. e.g. If an input has shape (2, 3, 100, 100)
+    and there is padding of 3, the padded input should be
+    of shape (2, 3, 106, 106).
+
+    When computing output sizes, you should discard incomplete rows
+    if the stride puts it over the edge. e.g. (2, 3, 5, 5) input,
+    3 kernels of size 2x2 and stride of 2 should result in an
+    output of shape (2, 3, 2, 2). (batch, output_channels, output_height, output_width)
+
+    You can expect sane sizes of things and don't have to explicitly error check
+    e.g. The kernel size will never be larger than the input size
+    or larger than the stride.
+    """
+
+    def im2col(self, matrix, kernel_size, stride, padding):
+        # Input: sptial block of image
+        # Output: columns of matrix
+        # Assuming filter is a square
+        #  In (batch, input_channels, height, width)
+        #  Out (input_channels*kernel*kernel, output_height*output_width*batch)
+        p=padding
+        original_shape = matrix.shape
+        paddedMatrix = np.pad(matrix, ((0,0),(0,0),(p,p),(p,p)), mode='constant')
+        nbatch, nchannels, height, width = np.shape(paddedMatrix)
+        outputHeight = int((height-kernel_size)/stride+1)
+        outputWidth = int((width-kernel_size)/stride+1)
+        output = np.zeros([ nchannels*(kernel_size**2), nbatch*outputWidth*outputHeight ])
+
+        # Different center for different kernel size
+        if (kernel_size%2==0):
+            offsetLower = 0
+            offsetUpper = kernel_size
+        else:
+            offsetLower = kernel_size//2
+            offsetUpper = kernel_size//2 +1
+
+        for k in range(nbatch):
+            counter = 0
+            batch = k*outputWidth*outputHeight
+            for i in range(0, height, stride):
+                for j in range(0, width, stride):
+                    V = np.pad(np.ones([original_shape[2], original_shape[3]]), ((p,p),(p,p)), mode='constant')
+                    if ((p-1)<j) and (j<(width-p)) and ((p-1)<i) and (i<(height-p)):
+                        if ((j+offsetUpper) <= width) and ((i+offsetUpper) <= height):
+                            patch = paddedMatrix[batch, 0, i-offsetLower:i+offsetUpper, j-offsetLower:j+offsetUpper].reshape(-1,1)
+                            for c in range(1, nchannels):
+                                patch = np.vstack((patch, paddedMatrix[batch, c, i-offsetLower:i+offsetUpper, j-offsetLower:j+offsetUpper].reshape(-1,1)))
+                            output[:, batch+counter] = patch.flatten()
+                            counter+= 1
+                            # For visualization (debug)
+                            if (False):
+                                V[ i-offsetLower:i+offsetUpper, j-offsetLower:j+offsetUpper]=2
+                                V[i,j]=3
+                                plt.axhline(i)
+                                plt.axvline(j)
+                                plt.imshow(V)
+                                plt.pause(0.1)
+        return output
+
+    def col2im(self, matrix, original_shape, kernel_size, stride, padding):
+        nbatch, nchannels, height, width = original_shape
+        paddedHeight = height+2*padding
+        paddedWidth = width+2*padding
+        paddedMatrix = np.zeros([nbatch*nchannels*paddedHeight*paddedWidth])
+        indices = np.array([ i for i in range(np.cumprod(original_shape)[-1])]).reshape(original_shape)
+        col_indices = im2col(indices, kernel_size, stride, padding).flatten()
+        matrix=matrix.flatten()
+        for i in range(len(col_indices)):
+            paddedMatrix[int(col_indices[i])] = matrix[i]
+        paddedMatrix = paddedMatrix.reshape([nbatch, nchannels, paddedHeight, paddedWidth])
+        if padding == 0:
+            return paddedMatrix
+        return paddedMatrix[:, :, padding:-padding, padding:-padding]
 
     @staticmethod
     @njit(parallel=True, cache=True)
@@ -23,10 +103,24 @@ class ConvLayer(Layer):
         # TODO
         # data is N x C x H x W
         # kernel is COld x CNew x K x K
-        return None
+
+        weights = np.moveaxis(weights, 1, -1)
+        weights_shape_moveaxis = weights.shape
+        weights = weights.reshape(-1, weights.shape[-1]) # (K K  COld) x CNew
+
+        colim = self.im2col()
+        output = np.matmul(weights.T, colim)+ bias # CNew x ( N x H x W)
+        output = self.col2im()
+
+        # Return to original shape
+        weights = weights.reshape(logits_shape_moveaxis)
+        weights = np.moveaxis(weights, -1, 1)
+        return output
 
     def forward(self, data):
         # TODO
+        self.input = data
+        output = forward_numba(self.input, self.weights.data, self.bias.data)
         return None
 
     @staticmethod
--- nn/layers/linear_layer_hw1.py
+++ nn/layers/linear_layer_hw1.py
@@ -10,29 +10,42 @@ class LinearLayer(Layer):
     def __init__(self, input_size: int, output_size: int, parent=None):
         super(LinearLayer, self).__init__(parent)
         self.bias = Parameter(np.zeros((1, output_size), dtype=np.float32))
-        self.weight = Parameter(np.zeros((input_size, output_size), dtype=np.float32))  # TODO create the weight parameter
-        self.input = None
+        self.weight = Parameter(np.zeros((input_size, output_size), dtype=np.float32)) # TODO create the weight parameter
+        self.input = 0
         self.initialize()
 
     def forward(self, data: np.ndarray) -> np.ndarray:
         """
-        Linear conv_layers (fully connected) forward pass
+        Linear layer (fully connected) forward pass
         :param data: n X d array (batch x features)
         :return: n X c array (batch x channels)
         """
-        # TODO do the linear conv_layers
+        # 3.1) TODO do the linear layer
+        # (n x d) (d x c) = (n x c)
+
         self.input = data # Save this for backward
         return np.matmul(data, self.weight.data)+self.bias.data
 
     def backward(self, previous_partial_gradient: np.ndarray) -> np.ndarray:
         """
         Does the backwards computation of gradients wrt weights and inputs
-        :param previous_partial_gradient: n X c partial gradients wrt future conv_layers
+        :param previous_partial_gradient: n X c partial gradients wrt future layer
         :return: gradients wrt inputs
         """
-        # TODO do the backward step
+        # 3.1) TODO do the backward step
+        # PrevGrd = (n x c)
+        # dW = (d x c)
+        # dB = (1 x c)
+        # dX = (n x d)
+
+        # Need to save gradient for dw and db
         self.bias.grad=np.sum(previous_partial_gradient, axis=0)
         self.weight.grad=np.matmul(self.input.T, previous_partial_gradient)
+
+        # Return dx
+        #  dy     dy          d(wx+b)     dy
+        # ---- = --------- X --------- = -------- X w^T
+        #  dx     d(wx+b)      dx         d(wx+b)
         return np.matmul(previous_partial_gradient, np.transpose(self.weight.data))
 
     def selfstr(self):
